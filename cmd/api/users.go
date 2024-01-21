8e6868dbe2f01fb4322d94e6efbb91fe9cec72ca
Shawky Elshalawy <85047354+shawkyelshalawy@users.noreply.github.com>
2024-01-21 09:31:47 +0200

    Fixing the user activation

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -85,22 +85,29 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 }
 
 func (app *application) activateUserHandler(w http.ResponseWriter, r *http.Request) {
-
+	// Parse the plaintext activation token from the request body.
 	var input struct {
-		TokenPlainText string `json:"token"`
+		TokenPlaintext string `json:"token"`
 	}
-	err := app.readJSON(w, r, input)
+
+	err := app.readJSON(w, r, &input)
 	if err != nil {
 		app.badRequestResponse(w, r, err)
 		return
 	}
+
+	// Validate the plaintext token provided by the client.
 	v := validator.New()
-	if data.ValidateTokenPlaintext(v, input.TokenPlainText); !v.Valid() {
+
+	if data.ValidateTokenPlaintext(v, input.TokenPlaintext); !v.Valid() {
 		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
 
-	user, err := app.models.Users.GetForToken(data.ScopeActivation, input.TokenPlainText)
+	// Retrieve the details of the user associated with the token using the
+	// GetForToken() method (which we will create in a minute). If no matching record
+	// is found, then we let the client know that the token they provided is not valid.
+	user, err := app.models.Users.GetForToken(data.ScopeActivation, input.TokenPlaintext)
 	if err != nil {
 		switch {
 		case errors.Is(err, data.ErrRecordNotFound):
@@ -111,7 +118,12 @@ func (app *application) activateUserHandler(w http.ResponseWriter, r *http.Reque
 		}
 		return
 	}
+
+	// Update the user's activation status.
 	user.Activated = true
+
+	// Save the updated user record in our database, checking for any edit conflicts in
+	// the same way that we did for our movie records.
 	err = app.models.Users.Update(user)
 	if err != nil {
 		switch {
@@ -122,6 +134,9 @@ func (app *application) activateUserHandler(w http.ResponseWriter, r *http.Reque
 		}
 		return
 	}
+
+	// If everything went successfully, then we delete all activation tokens for the
+	// user.
 	err = app.models.Tokens.DeleteAllForUser(data.ScopeActivation, user.ID)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
--- internal/data/users.go
+++ internal/data/users.go
@@ -159,8 +159,12 @@ func (m UserModel) Update(user *User) error {
 	return nil
 }
 
-func (m UserModel) GetForToken(tokenScope, tokenPlainText string) (*User, error) {
-	tokenHash := sha256.Sum256([]byte(tokenPlainText))
+func (m UserModel) GetForToken(tokenScope, tokenPlaintext string) (*User, error) {
+	// Calculate the SHA-256 hash of the plaintext token provided by the client.
+	// Remember that this returns a byte *array* with length 32, not a slice.
+	tokenHash := sha256.Sum256([]byte(tokenPlaintext))
+
+	// Set up the SQL query.
 	query := `
 			SELECT users.id, users.created_at, users.name, users.email, users.password_hash, users.activated, users.version
 			FROM users
@@ -169,10 +173,20 @@ func (m UserModel) GetForToken(tokenScope, tokenPlainText string) (*User, error)
 			WHERE tokens.hash = $1
 			AND tokens.scope = $2 
 			AND tokens.expiry > $3`
+
+	// Create a slice containing the query arguments. Notice how we use the [:] operator
+	// to get a slice containing the token hash, rather than passing in the array (which
+	// is not supported by the pq driver), and that we pass the current time as the
+	// value to check against the token expiry.
 	args := []any{tokenHash[:], tokenScope, time.Now()}
+
 	var user User
+
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
+
+	// Execute the query, scanning the return values into a User struct. If no matching
+	// record is found we return an ErrRecordNotFound error.
 	err := m.DB.QueryRowContext(ctx, query, args...).Scan(
 		&user.ID,
 		&user.CreatedAt,
